94dac073e44d89faa900257edc62d2b49e19e7c5
yusnof <yusnofal@gmail.com>
2025-02-04 09:54:26 +0100

    upp
    Co-authored-by: Simon <simonsimme@users.noreply.github.com>

--- src/main.s
+++ src/main.s
@@ -12,37 +12,30 @@ main:
      dst: .space 16
      src: .word 1,2,3,4,-1,-2,-3,-4
 
+    
+ 
 @copyelements
 copyelements: 
-    PUSH {}
-    LDR R0, =src
-    LDR R0, [R0]        
-    LDR R1, =dst
-    LDR R1, [R1]        
-    LDR R2, =size       
-    LDR R2, [R2]        
+    MOV R2, R3  @ calculate the size as end - start
+    MOV R3, #2
     
-    BL copyvec          
-    
-    POP {LR}            
+    BL copyvec              
+             
     BX LR               
-
-
 
 @copyvec
 copyvec:   PUSH {R4, R5} 
-           MOV R3, #0  @move 0 to r3 
+         //  MOV R3, #0  @ move 0 to r3 
 copyvec1:  CMP R3, R2
            BGE copyvec2
+           LSL R5, R3, #2
+           LDR R4, [R0, R5]
            LSL R5, R3, #1
-           LDRSH R4,[R1, R5]
-           LSL R5, R3,#2
-           STR R4, [R0, R5] @dest[i] = R4
-           ADD R3, R3, #1 @i++ we update the R3 with the new value 
-           B copyvec1  @we go back to the loop
+           STRH R4, [R1, R5] @ dest[i] = R4
+           ADD R3, R3, #1 @ i++ we update the R3 with the new value 
+           B copyvec1  @ we go back to the loop
 copyvec2:  POP {R4, R5} 
            BX LR 
-
 
     @ infinite loop to halt program execution
 .end_loop: 
